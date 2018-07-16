0ef544e7103067df2444db4cb82e9b969324ef3d
llqhz <llqhz@qq.com>
2018-07-16 12:45:50 +0800

    aaaa

--- src/llqhz/WeChat.php
+++ src/llqhz/WeChat.php
@@ -3,21 +3,21 @@
  * @Author: llqhz
  * @Date:   2018-07-16 10:11:37
  * @Last Modified by:   name
- * @Last Modified time: 2018-07-16 10:59:45
+ * @Last Modified time: 2018-07-16 12:41:26
  */
-/**
-*  WeChat
-*/
+namespace llqhz;
+
 class WeChat
 {
 
     function __construct($appId='',$appSecret='')
     {
         # code...
+        echo "llqhz=>wechat Init";
     }
 
     public function add()
     {
-        # code...
+        echo "llqhz -> add";
     }
 }
