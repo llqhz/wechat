3ec3c91817fdbfe014921f63949c9969e3adacbf
llqhz <llqhz@qq.com>
2018-07-16 10:16:06 +0800

    init wechat

---
+++ WeChat.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @Author: llqhz
+ * @Date:   2018-07-16 10:11:37
+ * @Last Modified by:   name
+ * @Last Modified time: 2018-07-16 10:13:04
+ */
+/**
+*  WeChat
+*/
+class WeChat
+{
+
+    function __construct($appId='',$appSecret='')
+    {
+        # code...
+    }
+}
