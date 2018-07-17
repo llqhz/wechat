bae7a4531226ba8a4c813178c25be8cad567e240
llqhz <llqhz@qq.com>
2018-07-17 10:31:17 +0800

    222

--- src/llqhz/WeChat.php
+++ src/llqhz/WeChat.php
@@ -3,7 +3,7 @@
  * @Author: llqhz
  * @Date:   2018-07-16 10:11:37
  * @Last Modified by:   name
- * @Last Modified time: 2018-07-16 12:41:26
+ * @Last Modified time: 2018-07-17 10:31:09
  */
 namespace llqhz;
 
